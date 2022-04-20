8c516dce30e50331707d9640f8e6dc4569205727
Zahra Al Bulushi <47219252+zahra98a@users.noreply.github.com>
2022-04-20 03:42:20 +0000

    Add files via upload

---
+++ app/src/main/java/com/example/supercookie/helper.java
@@ -0,0 +1,17 @@
+package com.example.supercookie;
+
+/*
+
+    curl --request POST https://connect.squareupsandbox.com/v2/payments \
+            --header "Content-Type: application/json" \
+            --header "Authorization: Bearer EAAAECBgx-N6pTBRwO9AtmigjgaedOpWljBdhQn2u8Wc-jQITnVx2DfqDvRKx2LL" \
+            --header "Accept: application/json" \
+            --data '{
+            "idempotency_key": "6bb8cff7-7fc6-488a-ac98-6a7ef8d4092f",
+            "amount_money": {
+        "amount": 100,
+                "currency": "EUR"},
+            "source_id": "cnon:CBASELYerUOFj0Folku34MjfGZA"}'
+
+
+*/
